e28ff95c498760ed5c06e35e49ca8f5e6ed539bd
Ivan Oparin <oparin@me.com>
2025-04-02 04:10:02 +0400

    Refactor to support new relux module

--- Sources/ReluxAnalytics/Analytics+Module.swift
+++ Sources/ReluxAnalytics/Analytics+Module.swift
@@ -2,18 +2,14 @@ import Relux
 
 extension Analytics {
     public final class Module: Relux.Module {
-        public let states: [any Relux.State]
-        public let uistates: [any Relux.Presentation.StatePresenting]
+        public let states: [any Relux.AnyState]
         public let sagas: [any Relux.Saga]
-        public let routers: [any Relux.Navigation.RouterProtocol]
         
         public init(
             sagas: [Analytics.Saga]
         ) {
             self.sagas = sagas
             self.states = []
-            self.uistates = []
-            self.routers = []
         }
     }
 }
